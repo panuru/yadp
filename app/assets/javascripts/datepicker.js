1817cc55196eef86bc8f3bfab4a13ecdf19ef62b
panuru <iseesomuch@gmail.com>
2014-03-18 09:11:01 +0400

    + datepicker as a rails app

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,68 @@
+//= require jquery
+//= require lodash
+//= require date
+//= require jquery_datepicker
+//= require templates
+//= require calendar
+(function($){
+  "use strict";
+
+  var templates = $.fn.datepicker.templates;
+
+  function DatePicker($input, options) {
+    this.$input = $input;
+    this.options = options = _.extend($.fn.datepicker.defaults, options);
+    var date = new Date($input.val());
+    // if not a valid date, use today's date
+    if(isNaN(date.getDay())) {
+      date = new Date();
+    }
+    this.date = date;
+
+    // replace dom element with a datepicker control
+    this.$button = $(templates.button({ 
+      value: date.toString(options.dateFormat) 
+    })).insertAfter($input.hide());
+
+    this.calendar = new $.fn.datepicker.Calendar(_.extend({ date: this.date }, this.options));
+
+    this.$button.click(this.toggle.bind(this));
+    this.calendar.on('date:click', this.setDate.bind(this));
+  }
+
+  DatePicker.prototype = {
+    isExpanded: function() {
+      return this.$button.hasClass('dp-expanded');
+    },
+    toggle: function() {
+      if (this.isExpanded()) {
+        this.calendar.remove();
+        this.$button.removeClass('dp-expanded').addClass('dp-collapsed');
+      } else {
+        this.calendar.render({ after: this.$button });
+        this.$button.removeClass('dp-collapsed').addClass('dp-expanded');
+      }
+    },
+    getDate: function() {
+      return this.date;
+    },
+    setDate: function(date){
+      date = new Date(date);
+      if(isNaN(date)) { 
+        throw new Error('Invalid date.'); 
+      }
+      this.date = date;
+      this.$button.html(date.toString(this.options.dateFormat));
+      this.$input.val(date.toDateString());
+      this.calendar.select(date);
+    },
+    destroy: function() {
+      this.calendar.destroy();
+      this.$button.remove();
+      this.$input.data('datepicker', undefined).show();
+    }
+  };
+
+  $.fn.datepicker.DatePicker = DatePicker;
+
+})(jQuery);
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yadp::Application.config.secret_key_base = 'b8d48191dab2b49e390e88d557c00faf323727a91d852fd0585adf561fa6090acdd4e1f060474e57af0b3a85b5f871dfb9a274ab3984b65980d6d544df8951f1'
